35d6bd4152992d763085249f457d00c51cf8efb6
Sama Durani <duranisama@gmail.com>
2024-08-06 14:03:01 -0400

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,21 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link,index) => {
+    return (
+      <a key={index} href={"#" + link}>{link}</a>
+    )
+  })
+
+  return <nav>{linkElements}</nav>;
+
+  // return (
+  //   <nav>
+  //     <a key={links[0]} href="#home">home</a>
+  //     <a key={links[1]} href="#about">about</a>
+  //     <a key={links[2]} href="#projects">projects</a>
+  //   </nav>
+  // )
 }
 
 export default NavBar;
